2d2cc429340c3b91c0a7f25cfba0c37aa79738e8
cakadi190 <cakadi190@gmail.com>
2025-06-18 22:56:40 +0700

    feat(notifications): require auth for notification routes

--- routes/api/v1/global.php
+++ routes/api/v1/global.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('image-optimize', \App\Http\Controllers\Api\v1\Global\ImageOptimizerController::class)->name('image-optimize');
 
-Route::prefix('notifications')->name('notifications.')->group(function () {
+Route::prefix('notifications')->name('notifications.')->middleware('auth')->group(function () {
     Route::get('/', [\App\Http\Controllers\Api\v1\Global\NotificationController::class, 'index'])->name('index');
     Route::get('refer', [\App\Http\Controllers\Api\v1\Global\NotificationController::class, 'referTo'])->name('referTo');
     Route::get('read-all', [\App\Http\Controllers\Api\v1\Global\NotificationController::class, 'readAll'])->name('readAll');
