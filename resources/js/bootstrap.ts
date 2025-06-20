36913d4b4a855152100d56386891101c76e8898b
cakadi190 <cakadi190@gmail.com>
2025-06-20 19:49:01 +0700

    feat(bootstrap): enable tooltips and popovers

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,20 +1,17 @@
 import axios from 'axios';
 import * as bootstrap from 'bootstrap';
+import {Tooltip} from "bootstrap";
 
 // Axios Config
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // Enabling Popover and Tooltip
-document.addEventListener('DOMContentLoaded', (): void => {
-  const tooltipTriggerList: HTMLElement[] | null = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-  const popoverTriggerList: HTMLElement[] | null = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+document.addEventListener('DOMContentLoaded',
+  (): void => {
+    const tooltipTriggerList: HTMLElement[] | null = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    const popoverTriggerList: HTMLElement[] | null = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
 
-  tooltipTriggerList.forEach((tooltipTriggerEl: HTMLElement) => {
-    new bootstrap.Tooltip(tooltipTriggerEl);
+    tooltipTriggerList.forEach((tooltipTriggerEl: HTMLElement): Tooltip => new bootstrap.Tooltip(tooltipTriggerEl));
+    popoverTriggerList.forEach((popoverTriggerEl: HTMLElement): Tooltip => new bootstrap.Popover(popoverTriggerEl));
   });
-
-  popoverTriggerList.forEach((popoverTriggerEl: HTMLElement) => {
-    new bootstrap.Popover(popoverTriggerEl);
-  });
-});
