a2b9573b9174bf779503dc706cd367ae81b21bed
cakadi190 <cakadi190@gmail.com>
2025-06-26 10:36:32 +0700

    docs(booking): add docblock to index method

--- app/Http/Controllers/Web/Admin/BookingController.php
+++ app/Http/Controllers/Web/Admin/BookingController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
+    /**
+     * Menampilkan halaman index booking.
+     *
+     * @return \Inertia\Response
+     */
     public function __invoke()
     {
         seo()->title('Data Peminjaman')->generate();
